614bb9338b28b99bf1dc6715cc8b0aab805e9cf9
davetanjung <davegtanjung@gmail.com>
2025-05-26 21:24:59 +0700

    crud itinerary done

--- src/applications/app.ts
+++ src/applications/app.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import router from "../routes/authRouter"
 import { authMiddleware } from "../middlewares/authMiddleware"
+import itineraryRouter from "../routes/itineraryRouter"
 
 const app = express()
 app.use(express.json())
-app.use(authMiddleware)
+// app.use(authMiddleware)
 app.use(router)
+app.use(itineraryRouter)
 
 export default app;
-
---
+++ src/models/Itinerary.ts
@@ -0,0 +1,44 @@
+export interface CreateItineraryRequest {
+    title: string;
+    start_date: string;
+    end_date: string;
+    estimate_start: string;
+    estimate_end: string;
+    total_person: number;
+    country: string;
+    location: string;
+    userId: number;
+}
+
+export interface UpdateItineraryRequest {
+    id: number;
+    title?: string;
+    start_date?: string;
+    end_date?: string;
+    estimate_start?: string;
+    estimate_end?: string;
+    total_person?: number;
+    country?: string;
+    location?: string;
+    userId?: number;
+}
+
+export interface DeleteItineraryRequest {
+    id: number;   
+}
+
+export interface ReadItineraryResponse {
+    title: string;
+    start_date: string;
+    end_date: string;
+    estimate_start: string;
+    estimate_end: string;
+    total_person: number;
+    country: string;
+    location: string;    
+}
+
+
+
+
+
