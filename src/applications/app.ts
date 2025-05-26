adf44c55e306c79d9b2d64e94ddba4ee7ebba75b
Aaron Asa <aaron.soelistiono22@gmail.com>
2025-05-26 22:32:42 +0700

    gacor

--- src/applications/app.ts
+++ src/applications/app.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "../middlewares/authMiddleware"
 
 const app = express()
 app.use(express.json())
-app.use(authMiddleware)
+// app.use(authMiddleware)
 app.use(router)
 
 export default app;
